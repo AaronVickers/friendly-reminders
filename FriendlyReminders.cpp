8943efc90dc8588ab609515e1660b3811ffd1210
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2023-04-11 18:47:58 +0100

    Empty BakkesMod plugin template

---
+++ FriendlyReminders.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "FriendlyReminders.h"
+
+
+BAKKESMOD_PLUGIN(FriendlyReminders, "write a plugin description here", plugin_version, PLUGINTYPE_FREEPLAY)
+
+std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
+
+void FriendlyReminders::onLoad()
+{
+	_globalCvarManager = cvarManager;
+	//LOG("Plugin loaded!");
+	// !! Enable debug logging by setting DEBUG_LOG = true in logging.h !!
+	//DEBUGLOG("FriendlyReminders debug mode enabled");
+
+	// LOG and DEBUGLOG use fmt format strings https://fmt.dev/latest/index.html
+	//DEBUGLOG("1 = {}, 2 = {}, pi = {}, false != {}", "one", 2, 3.14, true);
+
+	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
+	//	LOG("Hello notifier!");
+	//}, "", 0);
+
+	//auto cvar = cvarManager->registerCvar("template_cvar", "hello-cvar", "just a example of a cvar");
+	//auto cvar2 = cvarManager->registerCvar("template_cvar2", "0", "just a example of a cvar with more settings", true, true, -10, true, 10 );
+
+	//cvar.addOnValueChanged([this](std::string cvarName, CVarWrapper newCvar) {
+	//	LOG("the cvar with name: {} changed", cvarName);
+	//	LOG("the new value is: {}", newCvar.getStringValue());
+	//});
+
+	//cvar2.addOnValueChanged(std::bind(&FriendlyReminders::YourPluginMethod, this, _1, _2));
+
+	// enabled decleared in the header
+	//enabled = std::make_shared<bool>(false);
+	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
+
+	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
+	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
+	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
+	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&FriendlyReminders::FUNCTION, this, _1, _2, _3));
+	//gameWrapper->RegisterDrawable(bind(&TEMPLATE::Render, this, std::placeholders::_1));
+
+
+	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
+	//	LOG("Your hook got called and the ball went POOF");
+	//});
+	// You could also use std::bind here
+	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&FriendlyReminders::YourPluginMethod, this);
+}
---
+++ FriendlyReminders.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
+
+#include "version.h"
+constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
+
+
+class FriendlyReminders: public BakkesMod::Plugin::BakkesModPlugin
+	//,public SettingsWindowBase // Uncomment if you wanna render your own tab in the settings menu
+	//,public PluginWindowBase // Uncomment if you want to render your own plugin window
+{
+
+	//std::shared_ptr<bool> enabled;
+
+	//Boilerplate
+	void onLoad() override;
+	//void onUnload() override; // Uncomment and implement if you need a unload method
+
+public:
+	//void RenderSettings() override; // Uncomment if you wanna render your own tab in the settings menu
+	//void RenderWindow() override; // Uncomment if you want to render your own plugin window
+};
